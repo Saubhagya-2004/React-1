e85614e8079be7a237e302e28c9090436e43ed61
Saubhagya Baliarsingh <saubhagyabaliarsingh2@gmail.com>
2025-01-23 10:11:56 +0530

    changes

--- dist/index.5baa4167.js
+++ dist/index.5baa4167.js
@@ -3052,7 +3052,7 @@ const ResturantData = [
         ShopName: 'BurgeY',
         cuisine: 'American',
         rating: '4.1',
-        Image: 'https://th.bing.com/th/id/OIP.WQBi0m_4hgc8WXSRL-_ovAHaFH?w=289&h=200&c=7&r=0&o=5&dpr=1.3&pid=1.7'
+        Image: 'https://th.bing.com/th/id/OIP.RNEJRcChNOXRi0A6LU7KfAHaFE?w=286&h=195&c=7&r=0&o=5&dpr=1.3&pid=1.7'
     },
     {
         id: 3,
@@ -3060,7 +3060,7 @@ const ResturantData = [
         ShopName: 'Shiyan',
         cuisine: 'Japan',
         rating: '4.1',
-        Image: 'https://th.bing.com/th/id/OIP.WQBi0m_4hgc8WXSRL-_ovAHaFH?w=289&h=200&c=7&r=0&o=5&dpr=1.3&pid=1.7'
+        Image: 'https://th.bing.com/th/id/OIP.iKknrvDGBL6xd-vkTzcHpQHaE6?w=264&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'
     }
 ];
 const ResturantList = ()=>/*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {

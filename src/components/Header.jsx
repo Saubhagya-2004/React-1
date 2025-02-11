d009d683d1c0791d079199f19f6752630d40e80c
Saubhagya Baliarsingh <saubhagyabaliarsingh2@gmail.com>
2025-02-11 08:03:33 +0530

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
- const logedinUser=()=>{
+import { useState } from "react";
+const IsloggedIn=()=>{
     return true;
  }
   const Title=()=>(
@@ -9,18 +10,21 @@ alt="logo"
 src="https://th.bing.com/th/id/OIP.WV9sVQ5yJJ94ZqPoAWh7OAHaEK?w=280&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"/>
 </a>
 );
-const HeaderComponent=()=>(
-    <div className='header'>
-        <Title/>
-        <div className='nav-items' >
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact Us</li>
-                <li>Cart</li>
-            </ul>
+const HeaderComponent=()=>{
+    const[IsloggedIn,setIsloggedIn]=useState(true);
+    return (
+        <div className='header'>
+            <Title/>
+            <div className='nav-items' >
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+            {IsloggedIn ? (<button onClick={()=>{setIsloggedIn(false)}} >Logout</button>):(<button onClick={()=>{setIsloggedIn(true)}}>Login</button>)}
         </div>
-        {logedinUser()? <button>Logout</button>:<button>Login</button>}
-    </div>
-);
+    );
+};
 export default HeaderComponent;

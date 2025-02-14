e7232fa984d7a03fd4b0e9f900857aed64e854f4
Saubhagya Baliarsingh <saubhagyabaliarsingh2@gmail.com>
2025-02-14 06:58:45 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -1,35 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//default export
+// Default export
 import HeaderComponent from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
-//Named export
-// import { Title,HeaderComponent }  from './components/Header';
-//or
-// import HeaderComponent,{ Title } from './components/Header.jsx'; 
+import About from './components/About';
+import Error from './components/Error';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'; // Correct import from 'react-router-dom'
 
-// const Title=()=>(
-//     <img 
-//     className='logo'
-//     src='https://th.bing.com/th/id/OIP.WV9sVQ5yJJ94ZqPoAWh7OAHaEK?w=280&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7' alt='logo'/>
-// );
+// Named export
+// import { Title, HeaderComponent } from './components/Header';
+// or
+// import HeaderComponent, { Title } from './components/Header.jsx'; 
 
-const AppLayout=()=>(
+const AppLayout = () => (
     <>
-    <HeaderComponent/>
-    <Body/>
-    <Footer/>
+        <HeaderComponent />
+        <Body />
+        <Footer />
     </>
-)
-//react fragment
-// const jsx=(
-//     <React.Fragment>
-//     <h1>Jsx</h1>
-//     <h2>This is jsx</h2>
-//     </React.Fragment>
-    
-// );
+);
 
-const root=ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AppLayout/>);
+// Also write it under AppLayout component
+const appRouter = createBrowserRouter([
+    {
+        path: '/',
+        element: <AppLayout />,
+        errorElement: <Error/>,
+    },
+    {
+        path: '/about',
+        element: <About />
+    }
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<RouterProvider router={appRouter} />);
+// router{} => pass some configuration props
---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const About=()=>{
+    return(
+        <>
+        <div>About</div>
+        <h1>Hi</h1>
+        </>
+    )
+}
+export default About;

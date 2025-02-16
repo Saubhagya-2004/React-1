6ebf88b4688e566ad5a8031dfa99a859f498e1be
Saubhagya Baliarsingh <saubhagyabaliarsingh2@gmail.com>
2025-02-16 12:14:10 +0530

    changes

--- src/components/RestauMenu.jsx
+++ src/components/RestauMenu.jsx
@@ -1,22 +1,30 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Imageurl } from "./constant";
 const RestauMenu=()=>{
     const{id} =useParams();//destructuring
+    const [restaurant,setRestaurant]=useState([]);
     // console.log(params);
     useEffect(()=>{
         getrestaurantinfo();
     },[]);
+    
      async function getrestaurantinfo() {
         const data= await fetch("https://zuingy.mishra.codes/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&submitAction=ENTER&restaurantId=375041");
         // console.log(data);
         const json =await data.json();
-        console.log(json);
-        
+        const fetched=(json.data.cards);
+        console.log();
+        setRestaurant(fetched);
      }
     return(
         <div>
-        <h1>Restaurant id:{id}</h1>
-        <h2>Hlw</h2>
+        <h1>{restaurant.name}</h1>
+        <img src={Imageurl+restaurant.cloudinaryImageId} alt="" />
+        <h2>Restaurant id:{restaurant.id}</h2>
+        <h3>city : {restaurant.city}</h3>
+        <h3>AvgRating : {restaurant.avgRating} *</h3>
+        <h3>Cost:{restaurant.costForTwoMessage } </h3>
         </div>
     )
 }
--- src/components/constant.jsx
+++ src/components/constant.jsx
@@ -10,3 +10,4 @@ export const ResturantData = [
     
         
 ];
+export const Imageurl="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
